bdfc818ce55f56cc4d00011643f8bd3647616b8b
Nouman Ullah Siddiqui <nullah@macrosoftinc.com>
2022-06-30 19:48:08 +0500

    Adding DOA contracts

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x8f8ba558DEbf5d86eD2C2d00715CE896f0dA5cA4";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
